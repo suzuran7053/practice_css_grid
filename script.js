7047bbfb6cd839f1421911e08fdcc87d7c5d8574
Naoko Morris <naoko.morris1210@gmail.com>
2022-01-18 20:48:16 +0000

    Add Main Image into Header

--- script.js
+++ script.js
@@ -13,26 +13,33 @@ $("document").ready(function() {
     }
 
     function showSearchBar() {
-        $("header").css("height", "52.56px");
-        $("#hiddenSearchBar").css({
-            "display": "flex",
-            "justify-content": "space-between",
-            "align-items": "center",
-            "height": "100%"
-        });
-        $("#hiddenSearchBar input").css({
-            "width": "100%",
-            "padding": "4px",
-            "border-radius": "3px",
-            "height": "49px"
-        });
-        $("#searchForm").css({
-            "flex-grow": "2",
-            "border": "solid 1px white",
-            "border-radius": "3px",
-            "height": "100%"
-        });
-        $("#defaultMenu").css("display", "none");
+        let showSearchField = $("#hiddenSearchBar").css("display");
+        if (showSearchField == "none") {
+            $("header").css("height", "52.56px");
+            $("#hiddenSearchBar").css({
+                "display": "flex",
+                "justify-content": "space-between",
+                "align-items": "center",
+                "height": "100%"
+            });
+            $("#hiddenSearchBar input").css({
+                "width": "100%",
+                "padding": "4px",
+                "border-radius": "3px",
+                "height": "49px"
+            });
+            $("#searchForm").css({
+                "flex-grow": "2",
+                "border": "solid 1px white",
+                "border-radius": "3px",
+                "height": "100%"
+            });
+            $("#defaultMenu").css("display", "none");
+            $("header").css("height", "auto");
+        } else {
+            $("$hiddenSearchBar").css("display", "none");
+            $("#defaultMenu").css("display", "flex");
+        }
     }
 
     function closeSearchBar() {
